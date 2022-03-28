91a476d039a551a485ad577ee3befbe275585dbb
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-03-28 14:28:18 +0200

    added demonstrator function of libanimals to main()

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <main.hpp>
+#include <libanimals/cat.hpp>
 #include <liboperations/addition.hpp>
 #include <liboperations/subtraction.hpp>
 #include <libshapes/pointy/rectangle.hpp>
 #include <iostream>
+
+
+void demonstrate_libanimals_usage() {
+    animals::Cat cat("felix");
+    std::cout << "The cat's name is " << cat.getName() << ".\n\n";
+}
 
 
 void demonstrate_liboperations_usage() {
@@ -11,9 +18,8 @@ void demonstrate_liboperations_usage() {
     int sum = operations::add(a, b);
     int diff = operations::subtract(a, b);
     std::cout << "Adding " << a << " to " << b << " is equal to " << sum << ".\n";
-    std::cout << "Subtracting " << a << " from " << b << " is equal to " << diff << ".\n";
+    std::cout << "Subtracting " << a << " from " << b << " is equal to " << diff << ".\n\n";
 }
-
 
 
 void demonstrate_libshapes_usage() {
@@ -22,12 +28,13 @@ void demonstrate_libshapes_usage() {
     std::cout << "Creating a rect of width " << width << " and height " << height << "...\n";
     shapes::Rectangle rect(width, height);
     double area = rect.calcArea();
-    std::cout << "Its area is " << area << ".\n";
+    std::cout << "Its area is " << area << ".\n\n";
 }
 
 
 int main()
 {
+    demonstrate_libanimals_usage();
     demonstrate_liboperations_usage();
     demonstrate_libshapes_usage();
 }
