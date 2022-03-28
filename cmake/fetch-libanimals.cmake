09d8b8b0b539ef385e622a6e0f7524caa7824cf0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-03-28 14:47:01 +0200

    now fetching version 0.2.0 of libanimals

--- cmake/fetch-libanimals.cmake
+++ cmake/fetch-libanimals.cmake
@@ -6,7 +6,7 @@ include(FetchContent)
 FetchContent_Declare(
   libanimals
   GIT_REPOSITORY https://github.com/jspaaks/sandbox-cmake-cpp-libanimals.git
-  GIT_TAG        0.1.0
+  GIT_TAG        0.2.0
 )
 
 # CMake >= 3.14
